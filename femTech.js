8d5f7a1b070dd58caaec3604069514b8c5b76d4a
Jonas Almeida <jalmeida@mathbiol.org>
2017-06-19 11:24:54 -0400

    clean up for real

--- femTech.js
+++ femTech.js
@@ -1,34 +1,17 @@
-console.log('helloWold.js is loaded')
+console.log('femTech.js is loaded')
 
+/*
 x.onkeyup=y.onkeyup=function(ev){
     //if(ev.key=='Enter'){
         z.textContent=parseFloat(x.value)+parseFloat(y.value)
         //console.log('Enter was pressed for '+this.id+' at '+Date()+' and the value was '+this.value)
     //}
 }
-
-// playing with Plot.ly
-/*
-$.getJSON('https://data.cdc.gov/api/views/epev-k6ss/rows.json',function(dt){
-    console.log('this after loadind the data',dt)
-     var year=[], fert=[]
-     dt.data.forEach(function(r,i){
-         //console.log(r[10],r[9])
-         year[i]=parseFloat(r[9])
-         fert[i]=parseFloat(r[10])
-     });
-     pt = Plotly.plot( lala, [{
-         x:year,
-         y: fert }]
-               
-
-      )
-      4
-})
 */
+
 plotData = function(){
-    $.getJSON('https://data.cdc.gov/api/views/epev-k6ss/rows.json',function(dt){
-        console.log('this after loadind the data',dt)
+    p1 = $.getJSON('https://data.cdc.gov/api/views/epev-k6ss/rows.json',function(dt){
+        //console.log('this after loadind the data',dt)
          var year=[], fert=[]
          dt.data.forEach(function(r,i){
              //console.log(r[10],r[9])
@@ -42,7 +25,7 @@ plotData = function(){
          ft2=fert.slice(n+1,2*n)
          
 
-         pt1 = Plotly.plot( lala, [
+         pt1 = Plotly.plot( fig1, [
              {
                 x:yr,
                 y: ft1,
@@ -60,4 +43,7 @@ plotData = function(){
      })
  }
  plotData()
- console.log('is this before of after?')
+ //console.log('is this before of after?')
+
+ // loading multiple urls
+
